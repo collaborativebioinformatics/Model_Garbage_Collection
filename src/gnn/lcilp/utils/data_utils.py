c9db4a7d33f7b51cf9d3beb2a8329330196b178e
Yibei Chen <yibeichan@gmail.com>
2025-10-03 10:33:13 -0400

    fix: merge all training files in data_utils.py for adjacency list
    Previously, adjacency list was built only from triplets['train'], missing
    synthetic_train_* files. Now explicitly merges:
    - original_train
    - synthetic_train_0
    - synthetic_train_1
    - etc.
    
    This ensures the graph structure includes all training edges (original + synthetic)
    when building the adjacency matrices for message passing.
    
    Critical fix for proper original + synthetic data integration.

--- src/gnn/lcilp/utils/data_utils.py
+++ src/gnn/lcilp/utils/data_utils.py
@@ -16,10 +16,10 @@ def plot_rel_dist(adj_list, filename):
 
 
 def process_files(files, saved_relation2id=None):
-    '''
+    """
     files: Dictionary map of file paths to read the triplets from.
     saved_relation2id: Saved relation2id (mostly passed from a trained model) which can be used to map relations to pre-defined indices and filter out the unknown ones.
-    '''
+    """
     entity2id = {}
     relation2id = {} if saved_relation2id is None else saved_relation2id
 
@@ -29,10 +29,9 @@ def process_files(files, saved_relation2id=None):
     rel = 0
 
     for file_type, file_path in files.items():
-
         data = []
         with open(file_path) as f:
-            file_data = [line.split() for line in f.read().split('\n')[:-1]]
+            file_data = [line.split() for line in f.read().split("\n")[:-1]]
 
         for triplet in file_data:
             if triplet[0] not in entity2id:
@@ -47,18 +46,53 @@ def process_files(files, saved_relation2id=None):
 
             # Save the triplets corresponding to only the known relations
             if triplet[1] in relation2id:
-                data.append([entity2id[triplet[0]], entity2id[triplet[2]], relation2id[triplet[1]]])
+                data.append(
+                    [
+                        entity2id[triplet[0]],
+                        entity2id[triplet[2]],
+                        relation2id[triplet[1]],
+                    ]
+                )
 
         triplets[file_type] = np.array(data)
 
     id2entity = {v: k for k, v in entity2id.items()}
     id2relation = {v: k for k, v in relation2id.items()}
 
-    # Construct the list of adjacency matrix each corresponding to eeach relation. Note that this is constructed only from the train data.
+    # Construct the list of adjacency matrix each corresponding to each relation.
+    # Merge all training data (original_train + synthetic_train_*) for adjacency list
+    all_train_triplets = []
+    for file_type, file_triplets in triplets.items():
+        # Include 'train', 'original_train', and any 'synthetic_train_*' files
+        if (
+            file_type == "train"
+            or file_type == "original_train"
+            or file_type.startswith("synthetic_train")
+        ):
+            all_train_triplets.append(file_triplets)
+
+    # Concatenate all training triplets
+    if len(all_train_triplets) > 0:
+        merged_train = np.concatenate(all_train_triplets, axis=0)
+    else:
+        # Fallback to just 'train' if no matches found
+        merged_train = triplets.get("train", np.array([]))
+
     adj_list = []
     for i in range(len(relation2id)):
-        idx = np.argwhere(triplets['train'][:, 2] == i)
-        adj_list.append(csc_matrix((np.ones(len(idx), dtype=np.uint8), (triplets['train'][:, 0][idx].squeeze(1), triplets['train'][:, 1][idx].squeeze(1))), shape=(len(entity2id), len(entity2id))))
+        idx = np.argwhere(merged_train[:, 2] == i)
+        adj_list.append(
+            csc_matrix(
+                (
+                    np.ones(len(idx), dtype=np.uint8),
+                    (
+                        merged_train[:, 0][idx].squeeze(1),
+                        merged_train[:, 1][idx].squeeze(1),
+                    ),
+                ),
+                shape=(len(entity2id), len(entity2id)),
+            )
+        )
 
     return adj_list, triplets, entity2id, relation2id, id2entity, id2relation
 
@@ -67,4 +101,4 @@ def save_to_file(directory, file_name, triplets, id2entity, id2relation):
     file_path = os.path.join(directory, file_name)
     with open(file_path, "w") as f:
         for s, o, r in triplets:
-            f.write('\t'.join([id2entity[s], id2relation[r], id2entity[o]]) + '\n')
+            f.write("\t".join([id2entity[s], id2relation[r], id2entity[o]]) + "\n")
